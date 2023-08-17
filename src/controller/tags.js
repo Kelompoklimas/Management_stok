a55ece54d9d9bd1ab8cf1c88309c221f6c394a38
rajih <rajhk704@gmail.com>
2023-08-17 13:28:28 +0700

    add: Response Tags MongoDB

--- src/controller/tags.js
+++ src/controller/tags.js
@@ -1,4 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
+const { createResTagsMongo } = require("../service_mongo/tags");
 const Prisma = new PrismaClient();
 const Tags = Prisma.tags;
 
@@ -9,20 +10,25 @@ const allTags = async () => {
       name_tags: true,
     },
   });
-
-  return findAllTags;
+  if (findAllTags.length > 0) {
+    await createResTagsMongo("Success", "", JSON.stringify(findAllTags));
+    return findAllTags;
+  } else {
+    await createResTagsMongo("NotFound", "", "Tags Not Found");
+    return "NotFound";
+  }
 };
 
 async function createTags(p_name_category) {
   const tags = await Prisma.tags.create({
     data: {
       name_tags: p_name_category,
-    }
+    },
   });
-  return console.log ("succes create tags");
+  return console.log("succes create tags");
 }
 
-module.exports =  {
+module.exports = {
   allTags,
   createTags,
-}
+};
